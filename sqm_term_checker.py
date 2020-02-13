2b3d73ac1b958f5c73a343651402fdb6971c87a2
littale12 <littale12@gmail.com>
2020-02-13 18:29:12 -0500

    first debug

--- sqm_term_checker.py
+++ sqm_term_checker.py
@@ -1,9 +1,13 @@
 import pandas as pd 
 import re
 
-terms_list = pd.read_csv('terms_list.csv')
+terms_list = pd.read_csv('terms_list.csv', dtype=str)
 query_data = pd.read_csv('query_data.csv')
-output = brands
+query_data.astype
+output = terms_list
+
+#reindex might be unnecessary due to "for column in colmn_list" statement possibly 
+#creating new column in ouput file automatically but my knowledge is limited
 output.reindex(columns=[
 	'Impressions',
 	'Clicks',
@@ -11,19 +15,22 @@ output.reindex(columns=[
 	'Conversions',
 	'Conv. value',
 	])
-#might be unnecessary due to "for column in colmn_list" statement possibly 
-#creating new column in ouput file automatically but my knowledge is limited
+
 output.fillna(0)
 clmn_list = query_data.columns.tolist()
 clmn_list.pop(0)
-
+print(query_data.dtypes)
 #iterates through each check term in the terms_list, adds data from KPI rows for 
 #search term if check term is in search term as individual word
-for x in brands.iloc[:,0]:
-	for y in query_data['Search Term']:
+for x in terms_list.iloc[:,0]:
+	for y in query_data['Search term']:
 		if re.search(fr'\b{{x}}\b', y):
 			for column in clmn_list:
-				output.at(x, column) += query_data.at(y, column)
+				current_value = output.at(x, column) 
+				current_value += query_data.at(y, column)
+				x = 0
+				x+=1
+				print(x)
 
 output['ROAS'] = output['Conv. value']/output['Cost']
 output['Avg. CPC'] = output['Cost']/output['Clicks']
